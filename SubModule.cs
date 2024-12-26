73a49ce6d51b700b1393099c897e1a29bc0bd33f
Tadas Talalas <tadastalalas@gmail.com>
2024-12-26 06:52:29 +0200

    Add project files.

---
+++ SubModule.cs
@@ -0,0 +1,26 @@
+﻿using TaleWorlds.MountAndBlade;
+
+
+namespace Healing_On_Kill_Based_On_Medicine_Skill
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
